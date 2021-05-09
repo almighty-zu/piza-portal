228143553c34322436b96b7c30a5069a79f3796e
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-05-09 21:01:10 +0200

    Implement Material UI

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import classes from './PageNav.module.scss';
+
 
 const PageNav = props => {
   return (
-    <nav>
-      <NavLink exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>HomePage</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</NavLink>
-      <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</NavLink>
+    <nav className={classes.component}>
+      <Button className={classes.link} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>HomePage</Button>
+      <Button className={classes.link} component={NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+      <Button className={classes.link} component={NavLink} to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Button>
+      <Button className={classes.link} component={NavLink} to={`${process.env.PUBLIC_URL}/waiter`} activeClassName='active'>Waiter</Button>
+      <Button className={classes.link} component={NavLink} to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Button>
     </nav>
   );
 };
